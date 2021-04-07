652f33eef9be540185306aff6852c3b53817b8f4
Heison <hchan14@asu.edu>
2021-04-07 15:57:55 -0700

    finished signup and login pages

--- gamingProject/gamingProject.asmx.cs
+++ gamingProject/gamingProject.asmx.cs
@@ -12,7 +12,6 @@ using MySql.Data.MySqlClient;
 using System.Data;
 using System.Net;
 using System.Net.Mail;
-
 namespace gamingProject
 {
     /// <summary>
@@ -22,9 +21,39 @@ namespace gamingProject
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class WebService1 : System.Web.Services.WebService
     {
+        [WebMethod]
+        public Users[] ViewUsers()
+        {
+            DataTable sqlDt = new DataTable("Users");
+
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            string sqlSelect = "SELECT * FROM users";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            sqlDa.Fill(sqlDt);
+
+            List<Users> user = new List<Users>();
+            for (int i = 0; i < sqlDt.Rows.Count; i++)
+            {
+                user.Add(new Users
+                {
+                    user_id = Convert.ToInt32(sqlDt.Rows[i]["user_id"]),
+                    username = sqlDt.Rows[i]["username"].ToString(),
+                    email = sqlDt.Rows[i]["email"].ToString(),
+                    password = sqlDt.Rows[i]["password"].ToString(),
+                });
+            }
+            return user.ToArray();
+        }
+
+
+
 
         // creating an account with email & password
         [WebMethod]
@@ -60,7 +89,7 @@ namespace gamingProject
                         }
                     else if (e_str.Contains("email_UNIQUE"))
                     {
-                        return "The email addresss you entered is already in use.";
+                        return "The email address you entered is already in use.";
                     }
 
                     else
@@ -76,6 +105,40 @@ namespace gamingProject
                 return "The passwords do not match.";
             }
         }
+
+        // able to log in to the account
+        [WebMethod(EnableSession = true)]
+        public bool Login(string username, string password)
+        {
+            bool success = false;
+
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            string sqlSelect = "SELECT * FROM users WHERE username = @usernameValue AND password = @passwordValue";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@usernameValue", HttpUtility.UrlDecode(username));
+            sqlCommand.Parameters.AddWithValue("@passwordValue", HttpUtility.UrlDecode(password));
+
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            DataTable sqlDt = new DataTable("accounts");
+            sqlDa.Fill(sqlDt);
+
+            if (sqlDt.Rows.Count > 0)
+            {
+                // create session which allows to retrieve data among a particular user
+                Session["user_id"] = sqlDt.Rows[0]["user_id"];
+                Session["username"] = sqlDt.Rows[0]["username"];
+                Session["email"] = sqlDt.Rows[0]["email"];
+                Session["password"] = sqlDt.Rows[0]["password"];
+
+                success = true;
+            }
+            return success;
+        }
+
+
 
     }
 }
