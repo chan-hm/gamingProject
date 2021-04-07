776ef171ff22f79f8cea6db44c0588d0f1c7b734
Heison <hchan14@asu.edu>
2021-04-06 17:48:05 -0700

    created signup page

--- gamingProject/gamingProject.asmx.cs
+++ gamingProject/gamingProject.asmx.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Services;
 
+// to talk to mysql
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+// to manipulate data from a db
+using System.Data;
+using System.Net;
+using System.Net.Mail;
+
 namespace gamingProject
 {
     /// <summary>
@@ -17,10 +26,52 @@ namespace gamingProject
     public class WebService1 : System.Web.Services.WebService
     {
 
+        // creating an account with email & password
         [WebMethod]
-        public string HelloWorld()
+        public string CreateAccount(string username, string email, string password, string re_enter)
         {
-            return "Hello World";
+            // if password confirmation matches & email is never used, the account will be created
+            if (password == re_enter)
+            {
+                string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+                string sqlSelect = "INSERT INTO `pj2`.`users` (`username`, `email`, `password`) VALUES (@usernameValue, @emailValue, @passwordValue);";
+
+                MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+                MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@usernameValue", HttpUtility.UrlDecode(email));
+                sqlCommand.Parameters.AddWithValue("@emailValue", HttpUtility.UrlDecode(email));
+                sqlCommand.Parameters.AddWithValue("@passwordValue", HttpUtility.UrlDecode(password));
+
+                sqlConnection.Open();
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                    return "Account Created.";
+                }
+
+                catch (Exception e)
+                {
+                    var e_str = e.ToString();
+
+                    if (e_str.Contains("email_UNIQUE"))
+                    {
+                        return "The email addresss you entered is already in use.";
+                    }
+
+                    else
+                    {
+                        return e_str;
+                    }
+                }
+                sqlConnection.Close();
+            }
+
+            else
+            {
+                return "The password confirmation does not match.";
+            }
         }
+
     }
 }
---
+++ gamingProject/users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gamingProject
+{
+    public class Users
+    {
+        public int user_id;
+        public string username;
+        public string email;
+        public string password;
+    }
+}
